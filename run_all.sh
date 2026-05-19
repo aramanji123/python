a1aa067b6baf5c8f836f4f6b542eb50bdc4bd531
ANJI <113800925+aramanji123@users.noreply.github.com>
2026-05-19 09:00:49 +0530

    Create run_all.sh

---
+++ run_all.sh
@@ -0,0 +1,29 @@
+#!bin/usr/env bash
+
+set -euo pipefail
+
+#list  folders
+folders=("functions" "Numeric" "regex" "strings")
+
+for folder in "${folders[@]}"
+do 
+ echo "checking  the folder : $folder"
+ 
+ # Skip if folder does not exist
+  if [ ! -d $folder ];then
+  echo " foldernot found : $folder"
+  continue
+  fi
+
+  for file in "$folder"/*.py
+  do
+   [-f "$file" ] || continue
+
+   echo "executing $file "
+   python "$file"
+  done
+  done
+echo "all python files executed successfully"
+  
+
+
